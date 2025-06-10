d1c1767948bd623fb031c3db23c7ee63de81c93a
Pranjal Jawalkar <pranjaljawalkar06@gmail.com>
2025-06-10 11:39:24 +0530

    bug fixed

--- house of hearts/src/components/navbar.jsx
+++ house of hearts/src/components/navbar.jsx
@@ -4,7 +4,7 @@ const navbar = () => {
   return (
     <>
     <div className="navbar">
-  <div className="logo"><img src="src\assets\images\logo-white.png" alt="" /> <h3>Representing <br /> youth culture</h3></div>
+  <div className="logo"><img src="src\assets\images\logo-white.png" alt="" /><div className="tagline"><h3>Representing <br /> youth culture</h3></div> </div>
   <div className="nav-space"></div>
   <div className="nav-menu"><h6>MENU <i className="ri-close-fill"></i></h6></div>
   </div>
--- house of hearts/src/pages/home.jsx
+++ house of hearts/src/pages/home.jsx
@@ -1,68 +1,97 @@
-import React, { use } from 'react'
-import Navbar from '../components/navbar'
-import { useRef } from 'react';
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import CustomEase from 'gsap/CustomEase';
+import React, { use } from "react";
+import Navbar from "../components/navbar";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect } from "react";
+import { useState } from "react";
+import CustomEase from "gsap/CustomEase";
 gsap.registerPlugin(ScrollTrigger);
 const home = () => {
+  const videoscrollref = useRef(null);
+  useGSAP(()=>{
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".video-show",
+        start: "0% 100%",
+        end: "top 20%",
+        scrub: 1,
+        pin: ".showcase",
+      },
+    });
+    tl.to(".navbar", {
+      marginTop: "-1vh",
+      duration: 0.3,
+    })
+    tl.to(".logo h3",{
+      x: -200,
+     
+      duration: 0.3,
+    })
 
-useGSAP(() => {
-  gsap.timeline({
-    scrollTrigger: {
-      trigger: ".showcase",
-      start: " 30% 50%",
-      endTrigger: ".video-show",
-      end: "top 0%",
-      pin: ".showcase",
+    tl.to(".showcase", {
+     scale: 6.5,
       
-      scrub: 1,
       
-      markers: true,
-      
+      borderRadius: "1px",
+    })
+  })
+  const playVideo = () => {
+     videoscrollref.current.scrollIntoView({ behavior: 'smooth' });
+    const video= document.querySelector(".showcase video");
+    if( video.muted) {
+      video.muted= false;
+    } else {
+      video.muted = true;
     }
-  }).to(".showcase", {
-    left: "50%",
-    top: "50%",
-    transform: "translate(-50%, -50%)",
-    scale: 6,
    
-    borderRadius: "1px",
+  }
+  // useGSAP(() => {
+  //   gsap
+  //     .timeline({
+  //       scrollTrigger: {
+  //         trigger: ".video-show",
+  //         start: " 30% 60%",
+  //         end: "top 0%",
+  //         pin: ".showcase",
 
-  });
+  //         scrub: 1,
 
-});
+  //         markers: true,
+  //       },
+  //     })
+  //     .to(".showcase", {
+  //       left: "50%",
+  //       top: "50%",
+  //       transform: "translate(-50%, -50%)",
+  //       scale: 6,
 
- 
+  //       borderRadius: "1px",
+  //     });
+  // });
+
   return (
     <div>
-        <div className="homepage">
-            <Navbar />
-            <div className="home-text">
-              <h3>GAME CHANGING</h3>
-              <div className="center-txt">
-           <h3 className='side-text'>EVENTS</h3>
-           
-            <video className='showcase' autoPlay loop muted src="https://cdn.pixabay.com/video/2024/11/27/243647_large.mp4"></video>
-            
+      <Navbar />
+      <div className="homepage">
+        <div className="home-text">
+         
+          <h3>GAME CHANGING</h3>
+          <div className="center-txt">
+            <h3 className="side-text">EVENTS</h3>
+
+            <div className="showcase">
+              <video onClick={playVideo} autoPlay muted loop src="https://video-previews.elements.envatousercontent.com/h264-video-previews/20f5c172-4098-40d8-b64b-f3d50421b9ae/25324751.mp4"></video></div>
             <h3> AND</h3>
-
-            </div>
-           <h3 className='side-text'>EXPERIENCES</h3>
-
-            </div>
-           
-
+          </div>
+          <h3 className="side-text">EXPERIENCES</h3>
         </div>
-        <div className="video-show"></div>
-        <div className="specialities">
-
-        </div>
+      </div>
+      <div ref={videoscrollref} className="video-show"></div>
+      <div className="specialities"></div>
     </div>
-  )
-}
+  );
+};
 
-export default home
+export default home;
