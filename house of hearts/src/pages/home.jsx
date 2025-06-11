bff88a260b1cad547b9f21005513908147a212d1
Pranjal Jawalkar <pranjaljawalkar06@gmail.com>
2025-06-11 08:58:51 +0530

    team section done

--- house of hearts/src/pages/home.jsx
+++ house of hearts/src/pages/home.jsx
@@ -19,6 +19,7 @@ const home = () => {
         end: "top 20%",
         scrub: 1,
         pin: ".showcase",
+       
       },
     });
 
@@ -38,6 +39,70 @@ const home = () => {
       
       borderRadius: "1px",
     },["same"])
+   
+    //dusri timeline
+
+    const tl2= gsap.timeline({
+      scrollTrigger:{
+        trigger:".specialities",
+       pin:true,
+        markers:true,
+        start:"top 0%",
+        end:"bottom 0%",
+        scrub:0.5
+      }
+      
+    });
+    tl2.to(".word1",{
+      "--after-width": "100%", 
+      
+    })
+    tl2.from(".img-div1",{
+      width:"0vw"
+    })
+    tl2.to(".word2",{
+      "--secondword": "100%", 
+    
+    })
+    tl2.to(".word3",{
+      "--thirdword": "100%", 
+      
+    })
+    tl2.from(".img-div2",{
+      width:"0vw"
+    })
+    tl2.to(".word4",{
+      "--fourthword": "100%", 
+    })
+     tl2.to(".word5",{
+      "--fifthword": "100%", 
+    })
+    tl2.from(".img-div3",{
+      width:"0vw"
+    })
+     tl2.to(".word6",{
+      "--sixthword": "100%", 
+    })
+
+    //tisri timeline
+
+    const tl3=gsap.timeline({
+      scrollTrigger:{
+        trigger:".team",
+        start:"70% 70%",
+        endTrigger:".cards-container",
+        scrub:true,
+        pin:true,
+        markers:true
+      }
+    })
+
+    tl3.to(".cards-container",{
+      
+
+    top:"-140%"
+    })
+
   })
   const playVideo = () => {
      videoscrollref.current.scrollIntoView({ behavior: 'smooth' });
@@ -73,8 +138,9 @@ const home = () => {
   //     });
   // });
 
+
   return (
-    <div>
+    <div className="all">
       <Navbar />
       <div className="homepage">
         <div className="home-text">
@@ -91,7 +157,21 @@ const home = () => {
         </div>
       </div>
       <div ref={videoscrollref} className="video-show"></div>
-      <div ref={videostopref} className="specialities"></div>
+      <div ref={videostopref} className="specialities">
+        <div className="specs-firstl"><div className="word1">EXPERIENCE</div> <div className="img-div1"></div> <div className="word2">ARCHITECTS</div></div>
+         <div className="specs-secondl"><div className="word3">TACTIAL</div> <div className="img-div2"></div> <div className="word4">STORYTELLERS</div></div>
+          <div className="specs-thirdl"><div className="word5">AND CHANGE</div><div className="img-div3"></div><div className="word6">MAKERS</div> </div>
+      </div>
+      <div className="team"><p> ALWAYS <br />THE  TEAM</p>
+      <div className="cards-container">
+        <div className="card card1"></div>
+        <div className="card card2"></div>
+        <div className="card card3"></div>
+      </div>
+      
+     
+      </div>
+      <div className="events"></div>
     </div>
   );
 };
