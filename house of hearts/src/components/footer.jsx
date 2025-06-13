624069f2bbc0746b92ed2a669728a316c7583583
Pranjal Jawalkar <pranjaljawalkar06@gmail.com>
2025-06-13 18:04:34 +0530

    homepage all done

--- house of hearts/src/components/footer.jsx
+++ house of hearts/src/components/footer.jsx
@@ -5,7 +5,7 @@ const footer = () => {
     <div className='footer'>
         <div className="footer-logo">
           <div className="full-logo">
-            <img src="https://i.snapcdn.app/photo?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJodHRwczovL2luc3RhZ3JhbS5maGFuNS0xMC5mbmEuZmJjZG4ubmV0L3YvdDUxLjI4ODUtMTkvNTA1NDM5NDgzXzE3OTA5NTE0NTUyMTE4MTE3Xzg5MzEwMDI2MTg4MDYyNzQwMzBfbi5qcGc_c3RwPWRzdC1qcGdfczMyMHgzMjBfdHQ2Jl9uY19odD1pbnN0YWdyYW0uZmhhbjUtMTAuZm5hLmZiY2RuLm5ldCZfbmNfY2F0PTEwMSZfbmNfb2M9UTZjWjJRRVRHN29yelU1bVBfX1F2akhpd08wOGdSQm8xSzUycTFLaEw0RDZULTlkUjFPeHJVOTRVLXd2WmNsaFoycGdubTQmX25jX29oYz1wVWRWbnlIaW5sMFE3a052d0dlQUN1LSZfbmNfZ2lkPUdiYWZlcENIOFdpQTBqNXd3b01TX2cmZWRtPUFPUTFjMHdCQUFBQSZjY2I9Ny01Jm9oPTAwX0FmUGlkNUpteEw5T0hrWlp2VnNXYmhjSFBteE5UUGNjMnlTbmtwSDdEVDJjX1Emb2U9Njg1MEVDOTgmX25jX3NpZD04YjM1NDYiLCJmaWxlbmFtZSI6IlNuYXBJbnN0YS50b181MDU0Mzk0ODNfMTc5MDk1MTQ1NTIxMTgxMTdfODkzMTAwMjYxODgwNjI3NDAzMF9uLmpwZyIsIm5iZiI6MTc0OTc0NjY1MCwiZXhwIjoxNzQ5NzUwMjUwLCJpYXQiOjE3NDk3NDY2NTB9.fjGyRumSctvlo9PitzbRiOiDvt5lH79yC7ZJy7rFm9U" alt="" />
+            <img src="src\assets\images\hhh.jpg" alt="" />
             <h1>House Of Hearts</h1>
           </div>
           <div><span>connect with our socials</span></div>
